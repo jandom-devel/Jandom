fc956f84dfbf3fab3cce70c58fa414c317da2a29
Gianluca Amato <gamato@unich.it>
2012-04-03 12:17:25 +0000

    A new class encapsulating widening, narrowings and (in the future) other parameters for the analyzer.
    git-svn-id: http://sci138.sci.unich.it/svn/main/pca/Jandom/trunk@814 44c9cf90-bc9d-4a98-8a8d-f359a031bbce

---
+++ src/it/unich/sci/jandom/targets/Parameters.scala
@@ -0,0 +1,31 @@
+/**
+ * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+ * JANDOM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JANDOM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty ofa
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * (c) 2012 Gianluca Amato
+ */
+package it.unich.sci.jandom.targets
+
+import it.unich.sci.jandom.domains.NumericalProperty
+import it.unich.sci.jandom.domains.NumericalDomain
+
+/**
+ * This class is used to keep parameter for analyzers.
+ * @author Gianluca Amato <amato@sci.unich.it>
+ *
+ */
+class Parameters[Property <: NumericalProperty[Property]] (domain: NumericalDomain[Property]) {
+	var widening = domain.widening
+	var narrowing = domain.narrowing
+}
