7b0d5a35017a212518fecdcaf13d9f7a632d7c3f
Gianluca Amato <gamato@unich.it>
2012-05-03 16:20:09 +0000

    git-svn-id: http://sci138.sci.unich.it/svn/main/pca/Jandom/trunk@832 44c9cf90-bc9d-4a98-8a8d-f359a031bbce

---
+++ src/it/unich/sci/jandom/domains/parsers/PropertyParser.scala
@@ -0,0 +1,40 @@
+/**
+ * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+ * JANDOM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JANDOM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty ofa
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * (c) 2012 Gianluca Amato
+ */
+package it.unich.sci.jandom
+package domains.parsers
+
+import parsers.{LinearExpressionParser,LinearConditionParser}
+import targets.Environment
+import scala.util.parsing.combinator.JavaTokenParsers
+import domains.NumericalDomain
+import domains.NumericalProperty
+
+/**
+ * @author Gianluca Amato <amato@sci.unich.it>
+ *
+ */
+class PropertyParser extends JavaTokenParsers with LinearExpressionParser with LinearConditionParser  {
+	val env = Environment()
+	var closedVariables = false;
+    
+    protected val variable: Parser[Int] = ident ^^ { i => env.getBinding(i) match {
+      case Some(v) => v
+      case None => if (closedVariables) error("Unexpected variable") else env.addBinding(i)
+    }}	
+	def parse[Property<: NumericalProperty[Property]] (s: String, domain: NumericalDomain[Property]) = parseAll(condition,s).get.analyze(domain.full(env.size))
+}
