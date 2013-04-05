d7269506c3c4174e7e2e23349ca08540cf79d4b7
Gianluca Amato <gamato@unich.it>
2013-04-05 23:03:54 +0200

    Move SLILProgram out of the SLILStmt hierarchy
    A program is not a statement, so we adapt our classes. However,
    SLILProgram is now full of casts since statements and programs
    need different kind of parameters.

---
+++ core/src/main/scala/it/unich/sci/jandom/targets/slil/SingleStmt.scala
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2013 amato
+ *
+ * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+ * JANDOM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JANDOM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty ofa
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.unich.sci.jandom.targets.slil
+import AnalysisPhase.AnalysisPhase
+import it.unich.sci.jandom.domains.NumericalProperty
+
+/**
+ * This class represents a statement with two program points, one before and one after the 
+ * statement. 
+ * @author Gianluca Amato <gamato@unich.it>
+ */
+case class SingleStmt(stmt: SLILStmt) extends SLILStmt {
+  
+  override def analyzeStmt(params: Parameters)(input: params.Property, phase: AnalysisPhase, ann: Annotation[params.Property]): params.Property = {
+     ann((this,1)) = input
+     val result = stmt.analyzeStmt(params)(input, phase, ann)
+     ann((this,2)) = result
+     result
+  }
+
+  override def mkString[U <: NumericalProperty[_]](ann: Annotation[U], level: Int, ppspec: PrettyPrinterSpec) = {
+    val spaces = ppspec.indent(level)
+    val result = new StringBuilder()
+    if (ann.get(this,1) != None)  
+        result ++= spaces ++= ppspec.decorator(ann(this,1)) ++= "\n"
+    result ++= stmt.mkString(ann,level,ppspec)
+    if (ann.get(this,1) != None)  
+        result += '\n' ++= spaces ++= ppspec.decorator(ann(this,2))
+    result.toString
+  }
+}
