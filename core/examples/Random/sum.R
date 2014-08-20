36215431530c1b9f8e5da5af9317ae061b83db9d
Gianluca Amato <gamato@unich.it>
2014-08-20 15:52:39 +0200

    Add an example for the sum domain.

---
+++ core/examples/Random/sum.R
@@ -0,0 +1,19 @@
+# This example is tailored for the domain Interval + Parallelotopes
+# The wanted invariant inside the while loop is:
+# [ -1 <= x <= 0, -1 <= y <= 0, i = 0 ] + [ 0 <= i <= 10, x = i, y =i ]
+
+function () {
+  i = 0
+  x = 0
+  y = 0
+  while (i < 10) {
+    i = i+1
+    if (brandom()) {
+       x = i-1
+       y = i
+    } else {
+       x = i
+       y = i -1
+    }
+  }
+}
