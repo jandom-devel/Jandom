98a47c035f91a87967fd27613345c200748278f0
Gianluca Amato <gamato@unich.it>
2015-04-23 12:32:42 +0200

    Fix tests in JVMSootSuite.

--- core/src/test/ppl/it/unich/jandom/targets/JVMSootSuite.scala
+++ core/src/test/ppl/it/unich/jandom/targets/JVMSootSuite.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Gianluca Amato
+ * Copyright 2013 Gianluca Amato, Francesca Scozzari
  *
  * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
  * JANDOM is free software: you can redistribute it and/or modify
@@ -31,6 +31,7 @@ import it.unich.jandom.domains.objects.PairSharingDomain
 /**
  * Simple test suite for the JVMSoot target.
  * @author Gianluca Amato
+ * @author Francesca Scozzari
  *
  */
 class JVMSootSuite extends FunSuite with SootTests {
@@ -138,16 +139,29 @@ class JVMSootSuite extends FunSuite with SootTests {
       "longassignment" -> Set(),
       "topologicalorder" -> Set(),
       "complexif" -> Set(),
-      "objcreation" -> Set(UP(0, 0), UP(0, 5), UP(1, 1), UP(1, 5), UP(5, 6), UP(6, 7), UP(2, 2), UP(0, 1), UP(0, 6), UP(6, 6), UP(5, 7), UP(3, 6), UP(3, 5),
-        UP(1, 3), UP(1, 8), UP(2, 4), UP(0, 7), UP(5, 8), UP(3, 7), UP(0, 3), UP(1, 7), UP(7, 8), UP(8, 8), UP(0, 8), UP(4, 4), UP(5, 5), UP(7, 7), UP(3, 3),
-        UP(3, 8), UP(1, 6), UP(6, 8)),
-      "classrefinement" -> Set(UP(0, 0), UP(1, 1), UP(5, 6), UP(2, 2), UP(0, 1), UP(2, 3), UP(6, 6), UP(4, 5), UP(3, 6), UP(3, 5), UP(2, 4), UP(3, 4),
-        UP(2, 5), UP(4, 4), UP(5, 5), UP(3, 3)),
+      "objcreation" -> Set(
+          UP(0, 0), UP(0, 2), UP(0, 3), UP(0, 5), UP(0, 6), UP(0, 7), UP(0, 8),
+          UP(1, 1), UP(1, 4),
+          UP(2, 0), UP(2, 2), UP(2, 3), UP(2, 5), UP(2, 6), UP(2, 7), UP(2, 8),
+          UP(3, 3), UP(3, 5), UP(3, 6), UP(3, 7), UP(3, 8),
+          UP(4, 4),
+          UP(5, 5), UP(5, 6), UP(5, 7), UP(5, 8),
+          UP(6, 6), UP(6, 7), UP(6, 8),
+          UP(7, 7), UP(7, 8),
+          UP(8, 8)),
+      "classrefinement" -> Set(
+          UP(0, 0), UP(0, 3),
+          UP(1, 1), UP(1, 2), UP(1, 4), UP(1, 5),
+          UP(2, 2), UP(2, 4), UP(2, 5), UP(2, 6),
+          UP(3, 3),
+          UP(4, 4), UP(4, 5),
+          UP(5, 5), UP(5, 6),
+          UP(6, 6)),
       "class_parametric" -> Set(UP(0, 0), UP(0, 5), UP(1, 1), UP(1, 5), UP(2, 2), UP(0, 1), UP(0, 2), UP(3, 4), UP(1, 2), UP(2, 5), UP(4, 4), UP(5, 5), UP(3, 3)))
 
     for ((methodName, ps) <- jimplePairSharingTests) {
       val jmethod = new JimpleMethod(c.getMethodByName(methodName))
-      val params = new Parameters[JimpleMethod] {
+        val params = new Parameters[JimpleMethod] {
         val domain = new SootFrameObjectDomain(psdom)
         io = true
         //debugWriter = new java.io.PrintWriter(System.err)
