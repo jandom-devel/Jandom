db4d65493aa366ec28ddd4776c56d17f0a9274d0
Gianluca Amato <gamato@unich.it>
2015-06-15 10:33:56 +0200

    Add IterableFunction has a midstep between PartialFunction and Map.
    An IterableFunction is a partial function endowed with methods such as
    keys, keySets, etc.. like a Map. It is also an Iterable.

---
+++ core/src/test/scala/it/unich/jandom/utils/IterableFunctionSuite.scala
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2015 Gianluca Amato <gamato@unich.it>
+ *
+ * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+ * JANDOM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JANDOM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty ofa
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.unich.jandom.utils
+
+import org.scalatest.prop.PropertyChecks
+import org.scalatest.FunSpec
+
+class IterableFunctionSuite extends FunSpec with PropertyChecks {
+  describe("An empty iterable function") {
+    val f = IterableFunction.empty[Int,Int]
+    it("has empty domain") {
+      assert(f.isEmpty)
+    }
+  }
+
+  describe("An interable function derived from a map") {
+    it("has the same functional behaviour of the map") {
+      forAll { (m: Map[Int, Int]) =>
+        val f: IterableFunction[Int, Int] = m
+        for ( (x,y) <- m.toSeq) assertResult(y) { f(x) } 
+      }        
+    }
+  }
+}
