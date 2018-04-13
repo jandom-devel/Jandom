83a4a6b8221b50ea3bc677edf07d9bcc30771558
Tobia Tesan <tobia.tesan@gmail.com>
2018-04-13 23:07:20 +0200

    Add ApronDomain scratch tests

---
+++ core/src/test/apron/it/unich/jandom/domains/numerical/apron/ApronSpecification.scala
@@ -0,0 +1,166 @@
+/**
+  * Copyright 2018 Tobia Tesan
+  *
+  * This file is part of JANDOM: JVM-based Analyzer for Numerical DOMains
+  * JANDOM is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * JANDOM is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of a
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with JANDOM.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+package it.unich.jandom.domains.numerical.apron
+import it.unich.jandom.domains.numerical._
+import org.scalatest.PropSpec
+import org.scalatest.prop.PropertyChecks
+import org.scalacheck.Gen
+import spire.math.Rational
+import it.unich.jandom.domains.numerical._
+import it.unich.jandom.domains.numerical.LinearForm
+import it.unich.jandom.utils.numberext.RationalExt
+import scala.language.reflectiveCalls
+
+class ApronSpecification extends PropSpec with PropertyChecks {
+  import it.unich.jandom.domains.numerical.apron.Utils.Ints._
+  import it.unich.jandom.domains.numerical.apron.Utils.Rationals._
+  import it.unich.jandom.domains.numerical.apron.Utils.RationalExts._
+  import it.unich.jandom.domains.numerical.apron.Utils.LinearForms._
+
+  val apronDomain = ApronIntOctagonDomain()
+  ///////////////////////////////////////////////////////
+  // Properties of representation-converting helpers
+  ///////////////////////////////////////////////////////
+
+  property("coeffToRational . rationalToCoeff == id") {
+    forAll {
+      (r: Rational)
+      =>
+      assert(SpireGmpUtils.unsafeCoeffToRational(SpireGmpUtils.rationalToCoeff(r)) == r)
+    }
+  }
+
+  property("scalarToRatExt . ratExtToScalar == id") {
+    forAll {
+      (r: RationalExt)
+      =>
+      assert(SpireGmpUtils.scalarToRatExt(SpireGmpUtils.ratExtToScalar(r)) == r)
+    }
+  }
+
+  property("linexprToLf . lfToLinexpr == id") {
+    forAll {
+      (r: LinearForm)
+      =>
+      assert(SpireGmpUtils.linexprToLf(SpireGmpUtils.lfToLinexpr(r)) == r)
+    }
+  }
+
+  ///////////////////////////////////////////////////////
+  // Basic properties of Apron octagons
+  ///////////////////////////////////////////////////////
+
+
+  property("top.isTop") {
+    forAll (GenTinyPosInt){
+      n : Int
+      =>
+      val top = apronDomain.top(n)
+      assert (top.isTop)
+    }
+  }
+
+  property("bottom.isBottom") {
+    forAll (GenTinyPosInt){
+      n : Int
+      =>
+      val bottom = apronDomain.bottom(n)
+      assert (bottom.isBottom)
+    }
+  }
+
+  property("T U T = T & T U _|_ = T") {
+    forAll (GenTinyPosInt){
+      n : Int
+      =>
+      val top = apronDomain.top(n)
+      val bottom = apronDomain.bottom(n)
+      assert (top.union(top) == top)
+      assert (bottom.union(top) == top)
+    }
+  }
+
+  property("T /\\ _|_ = _|_ & _|_ /\\ _|_ = _|_") {
+    forAll (GenTinyPosInt){
+      n : Int
+      =>
+      val top = apronDomain.top(n)
+      val bottom = apronDomain.bottom(n)
+      assert (bottom.intersection(bottom) == bottom)
+      assert (top.intersection(bottom) == bottom)
+    }
+  }
+
+  property("T U _|_ >= _|_ & T /\\ _|_ =< T") {
+    forAll (GenTinyPosInt){
+      n : Int
+      =>
+      val top = apronDomain.top(n)
+      val bottom = apronDomain.bottom(n)
+      assert (bottom.union(top) > bottom)
+      assert (top.union(bottom) > bottom)
+      assert (bottom.intersection(top) < top)
+      assert (top.intersection(bottom) < top)
+      assert (bottom.union(top) >= bottom)
+      assert (top.union(bottom) >= bottom)
+      assert (bottom.intersection(top) <= top)
+      assert (top.intersection(bottom) <= top)
+    }
+  }
+
+  property("T != _|_, T !< T, _|_ !< _|_") {
+    forAll (GenTinyPosInt){
+      n : Int
+      =>
+      val top = apronDomain.top(n)
+      val bottom = apronDomain.bottom(n)
+      assert (!(top == bottom))
+      assert (!(bottom == top))
+      assert (!(top > top))
+      assert (!(top < top))
+      assert (!(bottom < bottom))
+      assert (!(bottom > bottom))
+    }
+  }
+
+  property("_|_ <= T.{x <- c} <= T ") {
+    import org.scalacheck.Shrink
+    implicit val noShrink: Shrink[Int] = Shrink.shrinkAny
+    forAll (Gen.choose(1, 50)){
+      n
+      =>
+      forAll (Gen.posNum[Int]){
+        ip
+        =>
+        forAll (Gen.choose(0, n)) {
+          x : Int =>
+          val i = ip%n
+          val top = apronDomain.top(n)
+          val bottom = apronDomain.bottom(n)
+          val lf = LinearForm.c(spire.math.Rational(x,1))
+          val updated = top.linearAssignment(i, lf)
+          assert((updated.toInterval).low(i) == x)
+          assert((updated.toInterval).high(i) == x)
+          assert(updated <= top)
+          assert(bottom <= updated)
+        }
+      }
+    }
+  }
+}
---
+++ core/src/test/apron/it/unich/jandom/domains/numerical/apron/Utils.scala
@@ -0,0 +1,162 @@
+package it.unich.jandom.domains.numerical.apron
+import it.unich.jandom.domains.numerical._
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Gen.Choose
+import org.scalacheck.Gen
+import spire.math.Rational
+import it.unich.jandom.utils.numberext.RationalExt
+
+object Utils {
+  val kTinyPosInt = 5
+  val kTinyNegInt = -5
+  val kMediumPosInt = 2^5
+  val kMediumNegInt = -2^5
+  val kLargePosInt = Int.MaxValue / (2^5)
+  val kLargeNegInt = Int.MinValue / (2^5)
+  val kLargestPosInt = Int.MaxValue
+  val kLargerstNegInt = Int.MinValue
+
+  object Ints {
+    def GenTinyInt : Gen[Int] = Gen.choose(kTinyNegInt, kTinyPosInt)
+    def GenTinyPosInt : Gen[Int] = Gen.choose(1, kTinyPosInt)
+    def GenMediumInt : Gen[Int] = Gen.choose(kMediumNegInt, kMediumPosInt)
+  }
+
+  object Rationals {
+
+    def GenRational: Gen[Rational] =
+      for {
+        n <- Gen.choose(kMediumNegInt, kMediumPosInt)
+        d <- Gen.choose(kMediumNegInt, kMediumPosInt).suchThat(_ != 0)
+      } yield {
+        Rational(n, d)
+      }
+
+    def GenPosRational: Gen[Rational] =
+      for {
+        n <- Gen.choose(1, kMediumPosInt)
+        d <- Gen.choose(1, kMediumPosInt)
+      } yield {
+        Rational(n, d)
+      }
+
+
+    def GenRationalInterval : Gen[(Rational, Rational)] = for {
+      a <- GenRational
+      b <- GenRational
+    } yield if (a <= b)
+      (a, b)
+    else (b, a)
+
+    implicit object chooseRat extends Choose[Rational] {
+      def choose(min: Rational, max: Rational): Gen[Rational] = {
+        assert(min.denominator > 0)
+        assert(max.denominator > 0)
+        assert(min <= max)
+        val lcm = spire.math.lcm(min.denominator, max.denominator)
+        // Careful with overflows here
+        val commonden = lcm
+        val minnum = min.numerator * (max.denominator / lcm)
+        val maxnum = max.numerator * (min.denominator / lcm)
+        assert(Rational(minnum, commonden) == min)
+        assert(Rational(maxnum, commonden) == max)
+        assert(minnum <= maxnum)
+        Gen.choose[Long](minnum.toLong, maxnum.toLong).map(n => Rational(n, commonden))
+      }
+    }
+
+    implicit def arbRational: Arbitrary[Rational] =
+      Arbitrary {
+        GenRational
+      }
+
+
+    def GenGreaterRat(r : Rational) = {
+      val arbnum = r.numerator * kMediumPosInt + 1 //arbitrary
+      val arbden = r.denominator * kMediumPosInt //arbitrary
+      val lower = Rational(arbnum, arbden)
+      assert(lower > r)
+      Gen.choose[Rational](lower, (lower + kMediumPosInt))
+    }
+
+    def GenSmallerRat(r : Rational) = {
+      val arbnum = r.numerator * kMediumPosInt - 1 //arbitrary
+      val arbden = r.denominator * kMediumPosInt //arbitrary
+      val upper = Rational(arbnum, arbden)
+      assert(upper < r)
+      Gen.choose[Rational]((upper - kMediumPosInt), upper)
+    }
+  }
+
+  object RationalExts {
+    def GenFiniteRationalExt = for (a <- Rationals.GenRational) yield RationalExt(a)
+    def GenFiniteRationalExtOrInf(pInf: Int = 10, pMinusInf: Int = 0) : Gen[RationalExt] = {
+      assert(pInf >= 0)
+      assert(pInf <= 100)
+      assert(pMinusInf >= 0)
+      assert(pMinusInf <= 100)
+      assert(pMinusInf + pInf <= 100)
+      Gen.frequency(
+        (pInf, Gen.const(RationalExt.PositiveInfinity)),
+        (pMinusInf, Gen.const(RationalExt.NegativeInfinity)),
+        (100 - pInf - pMinusInf, GenFiniteRationalExt)
+      )
+    }
+
+    implicit def arbRationalExt : Arbitrary[RationalExt] = Arbitrary { GenFiniteRationalExtOrInf(5, 5) }
+
+    def GenFiniteRationalExtInterval : Gen[(RationalExt, RationalExt)] = for {
+      a <- GenFiniteRationalExt
+      b <- GenFiniteRationalExt
+    } yield if (a <= b)
+      (a, b)
+    else (b, a)
+
+    def GenRationalExtInterval(pInf: Int = 5, pMinusInf: Int = 5) : Gen[(RationalExt, RationalExt)] = for {
+      a <- GenFiniteRationalExtOrInf(pInf, pMinusInf)
+      b <- GenFiniteRationalExtOrInf(pInf, pMinusInf)
+    } yield if (a <= b)
+      (a, b)
+    else (b, a)
+  }
+
+  object LinearForms {
+    def GenLfAnyDim : Gen[LinearForm] = for {
+      n <- Gen.choose(1,kTinyPosInt)
+      lf <- GenLf(n)
+    } yield lf
+
+    def GenLf(n:Int) : Gen[LinearForm] =
+      for {
+        coef <- Gen.containerOfN[Seq, Rational](n, Rationals.GenRational)
+      } yield LinearForm(coef :_*)
+
+    def GenExactLf(n: Int): Gen[LinearForm] = for
+    {
+      c <- Gen.choose(kMediumNegInt, kMediumPosInt)
+      vi <- Gen.choose(1, n)
+      ci <- Gen.oneOf(+1, -1)
+      vj <- Gen.choose(1, n)
+      cj <- Gen.oneOf(+1, -1) // TODO This can potentially overwrite vi, which is as intended since this yields the 1-coeff case, TODO: require different
+    } yield new DenseLinearForm(
+      Array.fill[Rational](n+1)(0)
+        .updated(0, Rational(c))
+        .updated(vi, Rational(ci))
+        .updated(vj, Rational(cj))
+        .toSeq
+    )
+    implicit def arbLinearForm: Arbitrary[LinearForm] =
+      Arbitrary { GenLfAnyDim }
+
+    def GenIntLfAnyDim : Gen[LinearForm] = for {
+      n <- Gen.choose(1,kTinyPosInt)
+      lf <- GenIntLf(n)
+    } yield lf
+
+    def GenIntLf(n:Int) : Gen[LinearForm] =
+      for {
+        coef <- Gen.containerOfN[Seq, Rational](n, Gen.choose[Int](kMediumNegInt, kMediumPosInt).map(Rational(_)))
+      } yield LinearForm(coef :_*)
+  }
+}
